3996b2db159f0e08e01d2cb4005a9b21b3e8a543
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-11-07 18:17:18 +0000

    Fix last, relative file paths

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ var inject = require('gulp-inject');
 
 gulp.task('default', function () {
 	return gulp.src('./index.html')
-		.pipe(inject(gulp.src('./js/*.js', {read: false})))
+		.pipe(inject(gulp.src('./js/*.js', {read: false}), {relative: true}))
 		.pipe(gulp.dest('./'));
 });
