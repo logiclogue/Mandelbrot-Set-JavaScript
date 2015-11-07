112c1ff9c1764dcd3f46de489035b979ede7c5ef
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-11-07 15:24:24 +0000

    Add Mandelbrot class

--- js/Main.js
+++ js/Main.js
@@ -1,27 +1,4 @@
-function mandelbrotPoint(a, b, aF, bF) {
-	return [
-		Math.pow(a, 2) - Math.pow(b, 2) + aF,
-		2 * a * b + bF
-	]
-}
-
-
-function mandelbrotIteration(a, b, max) {
-	var coord = mandelbrotPoint(a, b, a, b);
-
-	for (var i = 0; i < max; i += 1) {
-		coord = mandelbrotPoint(coord[0], coord[1], a, b);
-
-		if (coord[0] > 2 || coord[1] > 2) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-
 window.addEventListener("load", function ()
 {
-	console.log(mandelbrotIteration(0.12, 0.12, 100));
+	console.log(Mandelbrot.iterate([0.25, 0], 100));
 });
---
+++ js/Mandelbrot.js
@@ -0,0 +1,26 @@
+var Mandelbrot = new (function ()
+{
+	var self = this;
+
+
+	self.point = function (z, c) {
+		return [
+			Math.pow(z[0], 2) - Math.pow(z[1], 2) + c[0],
+			2 * z[0] * z[1] + c[1]
+		];
+	};
+
+	self.iterate = function (z, max) {
+		var coord = self.point(z, z);
+
+		for (var i = 0; i < max; i += 1) {
+			coord = self.point(coord, z);
+
+			if (coord[0] > 2 || coord[0] < -2 || coord[1] > 2 || coord[1] < -2) {
+				return false;
+			}
+		}
+
+		return true;
+	};
+});
